502be73301af08c1dc5d64daeb7a1488aa79388b
Pierre GILLON <contact@pierregillon.com>
2023-10-06 09:38:56 +0200

    tests: isolate task creation in a separate component

--- Blaztore.Examples.Wasm/Components/TodoListState.cs
+++ Blaztore.Examples.Wasm/Components/TodoListState.cs
@@ -4,12 +4,10 @@ using Blaztore.Examples.Wasm.Services;
 namespace Blaztore.Examples.Wasm.Components;
 
 public record TodoListState(
-    IReadOnlyCollection<TaskListItem> TodoListItems,
-    bool IsAddingTask,
-    string? NewTaskDescription
+    IReadOnlyCollection<TaskListItem> TodoListItems
 ) : IState
 {
-    public static TodoListState Initialize() => new(new List<TaskListItem>(), false, null);
+    public static TodoListState Initialize() => new(new List<TaskListItem>());
 
     public record Load : IAction<TodoListState>
     {
@@ -33,62 +31,6 @@ public record TodoListState(
                 state with
                 {
                     TodoListItems = action.Payload
-                };
-        }
-    }
-
-    public record StartAddingNewTask : IAction<TodoListState>
-    {
-        private record Reducer(IStore Store) : IPureReducer<TodoListState, StartAddingNewTask>
-        {
-            public TodoListState Reduce(TodoListState state, StartAddingNewTask action) =>
-                state with
-                {
-                    IsAddingTask = true
-                };
-        }
-    }
-
-    public record DefineNewDescription(string NewDescription) : IAction<TodoListState>
-    {
-        private record Reducer(IStore Store) : IPureReducer<TodoListState, DefineNewDescription>
-        {
-            public TodoListState Reduce(TodoListState state, DefineNewDescription action) =>
-                state with
-                {
-                    NewTaskDescription = action.NewDescription
-                };
-        }
-    }
-
-    public record ExecuteTaskCreation : IAction<TodoListState>
-    {
-        private record Effector(IStore Store, ITodoListApi Api, IActionDispatcher ActionDispatcher)
-            : IEffect<TodoListState, ExecuteTaskCreation>
-        {
-            public async Task Effect(TodoListState state, ExecuteTaskCreation action)
-            {
-                if (string.IsNullOrWhiteSpace(state.NewTaskDescription))
-                {
-                    return;
-                }
-
-                await Api.Create(Guid.NewGuid(), state.NewTaskDescription);
-                await ActionDispatcher.Dispatch(new EndAddingNewTask());
-                await ActionDispatcher.Dispatch(new Load());
-            }
-        }
-    }
-
-    public record EndAddingNewTask : IAction<TodoListState>
-    {
-        private record Reducer(IStore Store) : IPureReducer<TodoListState, EndAddingNewTask>
-        {
-            public TodoListState Reduce(TodoListState state, EndAddingNewTask action) =>
-                state with
-                {
-                    NewTaskDescription = null,
-                    IsAddingTask = false
                 };
         }
     }
